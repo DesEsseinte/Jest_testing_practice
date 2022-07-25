69f9018ef253447ee28bf858376ee76a9498ecd4
robert <matthewrueter51@gmail.com>
2022-07-25 13:16:51 -0500

    add year vs years test.

--- code/checkUserAge.js
+++ code/checkUserAge.js
@@ -1,7 +1,24 @@
 
 
-checkUserAge = (name,array){
+function checkUserAge (name){
+	let message;
 	
+	const myArray = [
+		{name: "Matt", age: 41},
+		{name: "Victoria", age: 46},
+		{name: "Pink Teddy", age: 45},
+		{name: "Bumble", age:1}
+	]
+
+	for(let i=myArray.length-1; i>=0;i--){
+		if (myArray[i].name === name){
+			message = `${name} is ${myArray[i].age} years old.`
+		}
+		if(myArray[i].age === 1){
+			message = `${name} is ${myArray[i].age} year old.`
+		}
+	}
+	return message;
 }
 
 module.exports = checkUserAge;
--- tests/checkUserAge.test.js
+++ tests/checkUserAge.test.js
@@ -4,3 +4,9 @@ const checkUserAge = require('../code/checkUserAge');
 test('checkUserAge returns message with name and age of user', () =>{
 	expect(checkUserAge('Matt')).toBe('Matt is 41 years old.');
 });
+test("checkUserAge returns 'year' not 'years' if age is 1", () =>{
+	expect(checkUserAge('Bumble')).toBe('Bumble is 1 year old.')
+});
+test('CheckUserAge returns -user not found- message correctly', () =>{
+	expect(checkUserAge('Henry')).toBe('User not found.')
+})
