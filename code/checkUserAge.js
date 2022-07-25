cdcaf977c60d6aee053f0c4e82f0f8399565a6ee
robert <matthewrueter51@gmail.com>
2022-07-25 16:31:44 -0500

    checkUserAge passes all tests.

--- code/checkUserAge.js
+++ code/checkUserAge.js
@@ -1,24 +1,34 @@
 
+	const myArray = [
+		{name: "matt", age: 41, message: "Matt is 41 years old."},
+		{name: "victoria", age: 46, message: "Victoria is 46 years old."},
+		{name: "pink teddy", age: 45, message: "Pink Teddy is 45 years old."},
+		{name: "bumble", age:1, message: "Bumble is 1 year old."}
+	]
 
 function checkUserAge (name){
-	let message;
+	let message = null;
 	
-	const myArray = [
-		{name: "Matt", age: 41},
-		{name: "Victoria", age: 46},
-		{name: "Pink Teddy", age: 45},
-		{name: "Bumble", age:1}
-	]
+	if(name.length>0){
+		name = name.toLowerCase();
+	}else{
+		
+		return "specify a name."
+	}
+
 
 	for(let i=myArray.length-1; i>=0;i--){
 		if (myArray[i].name === name){
-			message = `${name} is ${myArray[i].age} years old.`
-		}
-		if(myArray[i].age === 1){
-			message = `${name} is ${myArray[i].age} year old.`
+			message = myArray[i].message
 		}
 	}
+
+	if(!message){
+		message = "User not found."
+	}
+
 	return message;
+	
 }
 
 module.exports = checkUserAge;
--- tests/checkUserAge.test.js
+++ tests/checkUserAge.test.js
@@ -1,12 +1,27 @@
 const checkUserAge = require('../code/checkUserAge');
 
 
-test('checkUserAge returns message with name and age of user', () =>{
+test("checkUserAge sends message with user's age - Matt", () =>{
 	expect(checkUserAge('Matt')).toBe('Matt is 41 years old.');
 });
-test("checkUserAge returns 'year' not 'years' if age is 1", () =>{
-	expect(checkUserAge('Bumble')).toBe('Bumble is 1 year old.')
+test("checkUserAge sends message with user's age - Victoria", () =>{
+	expect(checkUserAge('Victoria')).toBe('Victoria is 46 years old.');
 });
+
+test('checkUserAge works with names of two+ words -Pink Teddy', () =>{
+	expect(checkUserAge('Pink Teddy')).toBe('Pink Teddy is 45 years old.');
+});
+
 test('CheckUserAge returns -user not found- message correctly', () =>{
 	expect(checkUserAge('Henry')).toBe('User not found.')
-})
+});
+test('CheckUserAge returns specify name for empty string.', () =>{
+	expect(checkUserAge('')).toBe('specify a name.')
+});
+
+test("CheckUserAge can handle lower and uppercase searches- A",() =>{
+	expect(checkUserAge("matt")).toBe('Matt is 41 years old.');
+});
+test("CheckUserAge can handle lower and uppercase searches- B",() =>{
+	expect(checkUserAge("MaTt")).toBe('Matt is 41 years old.');
+});
