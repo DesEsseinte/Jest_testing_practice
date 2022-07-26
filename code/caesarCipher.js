9b61259f370e9b59abfe70257f88802d29cf6fdd
robert <matthewrueter51@gmail.com>
2022-07-26 11:54:02 -0500

    add tests for numbers and capitals.

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,4 +1,4 @@
-const cipher 	= 	"abcdefghijklmnopqrstuvwxyz"
+const cipher 	= 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
 function caesarCipher (word){
@@ -10,11 +10,21 @@ function caesarCipher (word){
 	for(let i=0; i<=word.length-1; i++){
 		for(let index=0; index <=cipher.length-1; index++){
 			if(word[i]===cipher[index]){
-								
-				encryptedWord += cipher[index+1];
+				if(word[i]==="z"){
+					encryptedWord += "a"
+				}else if(word[i]==="Z"){
+					encryptedWord += "A"
+				}else if(word[i]==="9"){
+					encryptedWord += "0"
+				}else{
+
+					encryptedWord += cipher[index+1];
+
+				}			
 			}
 		}
 	}
+
 	return encryptedWord;
 }
 
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -4,19 +4,27 @@ const caesarCipher = require ('../code/caesarCipher')
 test("caesarCipher returns input shifted up by one letter", () =>{
 	expect(caesarCipher("matt")).toBe("nbuu")
 });
-test("caesarCipher returns input shifted up by one letter", () =>{
+test("caesarCipher handles various inputs made up of letters", () =>{
 	expect(caesarCipher("abc")).toBe("bcd")
 });
 test("caesarCipher throws error if input is empty", () =>{
 	expect( ()=> caesarCipher("")).toThrow(Error)
 }); 
-
-test("caesarCipher can accept a word with 'z'", () =>{
+test("caesarCipher accepts words with 'z'", () =>{
 	expect(caesarCipher("zebra")).toBe("afcsb")
 });
-test("caesarCipher can accept numbers", () =>{
+test("caesarCipher accepts numbers", () =>{
 	expect(caesarCipher("hello1")).toBe("ifmmp2")
 });
-test("caesarCipher can accept number 9", () =>{
+test("caesarCipher accepts number 9", () =>{
 	expect(caesarCipher("hello9")).toBe("ifmmp0")
+});
+test("caesarCipher accepts words alternating numbers and letters", () =>{
+	expect(caesarCipher("09ma22tt")).toBe("10nb33uu")
+});
+test("caesarCipher accepts words with capital letters", () =>{
+	expect(caesarCipher("MATT")).toBe("NBUU")
+});
+test("caesarCipher works with capital 'Z'", () =>{
+	expect(caesarCipher("Zebra")).toBe("Afcsb")
 });
