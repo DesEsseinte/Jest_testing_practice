6ace1d6eaf45492b55ad0d97da55b66f34879010
robert <matthewrueter51@gmail.com>
2022-07-26 11:22:27 -0500

    create tests for numbers and letter z.

--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -10,3 +10,13 @@ test("caesarCipher returns input shifted up by one letter", () =>{
 test("caesarCipher throws error if input is empty", () =>{
 	expect( ()=> caesarCipher("")).toThrow(Error)
 }); 
+
+test("caesarCipher can accept a word with 'z'", () =>{
+	expect(caesarCipher("zebra")).toBe("afcsb")
+});
+test("caesarCipher can accept numbers", () =>{
+	expect(caesarCipher("hello1")).toBe("ifmmp2")
+});
+test("caesarCipher can accept number 9", () =>{
+	expect(caesarCipher("hello9")).toBe("ifmmp0")
+});
