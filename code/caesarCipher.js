23d21e4d96045546717b1aecd3fa67955f5f205c
robert <matthewrueter51@gmail.com>
2022-07-25 20:01:33 -0500

    Caesar cipher function passes basic tests.

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,13 +1,21 @@
-const original 	= 	"abcdefghijklmnopqrstuvwxyz";
-const cipher 	= 	"bcdefghijklmnopqrstuvwzyza"
+const cipher 	= 	"abcdefghijklmnopqrstuvwxyz"
+
 
 function caesarCipher (word){
-	
 	if(word.length===0){
 		throw new Error("You must enter a word.")
 	}
+	let encryptedWord ="";
 
-	return 
+	for(let i=0; i<=word.length-1; i++){
+		for(let index=0; index <=cipher.length-1; index++){
+			if(word[i]===cipher[index]){
+								
+				encryptedWord += cipher[index+1];
+			}
+		}
+	}
+	return encryptedWord;
 }
 
 module.exports = caesarCipher;
